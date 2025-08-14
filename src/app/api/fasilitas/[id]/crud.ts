6a3d7b952786894455ceb9e0fc6c56c45075c5d0
Alwan WZ <alwanwildan942@gmail.com>
2025-08-14 23:50:05 +0700

    update crud.ts dan route.ts

--- src/app/api/fasilitas/[id]/crud.ts
+++ src/app/api/fasilitas/[id]/crud.ts
@@ -5,15 +5,9 @@ import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 // PUT /api/fasilitas/[id]
 
-<<<<<<< HEAD
 export async function PUT(req: NextRequest, context: { params: Record<string, string> }) {
   try {
-  const id = context.params.id;
-=======
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
-  try {
     const id = context.params.id;
->>>>>>> 42a4b6686bd62956ef12b62fa1968da6f1147c7a
     const data = await req.json();
     await updateDoc(doc(db, "fasilitas", id), data);
     return NextResponse.json({ success: true });
@@ -24,15 +18,9 @@ export async function PUT(req: NextRequest, context: { params: { id: string } })
 
 // DELETE /api/fasilitas/[id]
 
-<<<<<<< HEAD
 export async function DELETE(_req: NextRequest, context: { params: Record<string, string> }) {
   try {
-  const id = context.params.id;
-=======
-export async function DELETE(_req: NextRequest, context: { params: { id: string } }) {
-  try {
     const id = context.params.id;
->>>>>>> 42a4b6686bd62956ef12b62fa1968da6f1147c7a
     await deleteDoc(doc(db, "fasilitas", id));
     return NextResponse.json({ success: true });
   } catch (err) {
